b3200fe61e6d32e94bba24175fca94240a27e05c
Franck Theeten <franck.theeten@africamuseum.be>
2022-03-16 11:34:22 +0100

    Db version init

--- darwin/data/db/changes/0025-elevation-and-other-gtu-things.sql
+++
@@ -1,5 +0,0 @@
-SET search_path = darwin2, public;
-
-\i ../maintenance/recreate_flat.sql
-
-\i ../createfunctions.sql
--- darwin/data/db/changes/0031-db_version.sql
+++
@@ -1,9 +0,0 @@
-create table db_version (
- id integer not null,
- update_at timestamp default now()
- 
-);
-
-comment on table db_version is 'Table holding the database version and update date';
-
-insert into db_version(id) values(31);
