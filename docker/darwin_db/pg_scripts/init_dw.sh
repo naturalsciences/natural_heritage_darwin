86087f06aea75e58cfa1596ed246b16b30850ffa
Franck Theeten <franck.theeten@africamuseum.be>
2024-07-15 15:29:01 +0200

    Added files for docker depolyment

---
+++ docker/darwin_db/pg_scripts/init_dw.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+su postgres << 'EOF'
+ CREATE database darwin2;
+ Create user darwin2 with password 'darwin2';
+ Create user d2viewer with password 'd2viewer';
+ Create user ipt_viewer with password 'ipt_viewer';
+ create extension postgis;
+create extension hstore;
+create extension pg_trgm;
+create extension "uuid-ossp";
+create extension fuzzystrmatch;
+create extension pgcrypto;
+CREATE EXTENSION plpython3u;
+
+ALTER ROLE darwin2 SET search_path = darwin2,public;
+ALTER DATABASE darwin2 SET datestyle ='dmy';
+
+CREATE AGGREGATE array_concat_agg(anycompatiblearray) (
+   SFUNC = array_cat,
+   STYPE = anycompatiblearray
+);
+EOF
